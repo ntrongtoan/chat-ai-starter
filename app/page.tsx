60c2df3ce3106fc4b8dbf1081bb99957cafe1f44
Toan Nguyen <toan.nt181@gmail.com>
2025-05-11 10:30:20 +0700

    feat: demo page app

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,130 @@
-import Image from "next/image";
+"use client";
+
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
+import {
+  AppWindowMac,
+  AudioLines,
+  BotMessageSquare,
+  Github,
+  Mail,
+  MessageCircleQuestion,
+} from "lucide-react";
+import { cn } from "@/lib/utils";
+
+const iconClassName =
+  "rounded-full border bg-card text-card-foreground shadow-xl p-4";
+
+const cardClassName =
+  "rounded-lg bg-card text-card-foreground shadow-demo p-8 space-y-4 flex flex-col items-center justify-center";
+
+const textHeading1ClassName =
+  "bg-gradient-to-r from-purple-500 to-purple-900 bg-clip-text text-transparent";
+const textHeading2ClassName =
+  "bg-gradient-to-r from-fuchsia-600 to-pink-600 bg-clip-text text-transparent";
+const textHeading3ClassName =
+  "bg-gradient-to-r from-slate-300 to-slate-500 bg-clip-text text-transparent";
+const bgGradient2ClassName = "bg-gradient-to-tr from-violet-50 to-pink-50";
 
 export default function Home() {
+  const [title, setTitle] = useState("Chat Toolkit");
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div className={cn("flex min-h-screen", bgGradient2ClassName)}>
+      <div className="space-y-4 p-4 flex-1 shadow-demo">
+        <div className="grid grid-cols-3 gap-4">
+          <div className={cn(cardClassName, "relative group")}>
+            <div className="space-y-1">
+              <p className="text-3xl font-medium text-center">
+                <span
+                  className={cn("text-muted-foreground", textHeading2ClassName)}
+                >
+                  Chat Toolkit
+                </span>
+              </p>
+              <p className="text-md font-medium text-center">
+                built by{" "}
+                <span className={textHeading1ClassName}>ToanNguyen</span>
+              </p>
+            </div>
+            <div className="flex gap-2 items-center justify-center">
+              <div className={iconClassName}>
+                <BotMessageSquare />
+              </div>
+              <div className={iconClassName}>
+                <AudioLines />
+              </div>
+              <div className={iconClassName}>
+                <AppWindowMac />
+              </div>
+              <div className="rounded-lg group-hover:opacity-100 opacity-0 absolute top-0 left-0 right-0 bottom-0 backdrop-blur-md bg-opacity-30 bg-black/5 transition-opacity duration-300">
+                <div className="group-hover:opacity-100 opacity-0 absolute top-1/2 -translate-y-1/2 left-8 right-8 transition-opacity duration-300 space-y-2">
+                  <div className="flex gap-2 text-md items-center">
+                    <div className="rounded-full border bg-card text-card-foreground shadow-xl p-2 backdrop-blur-md bg-opacity-30 border-opacity-50">
+                      <Github />
+                    </div>
+                    <a
+                      href="https://github.com/ntrongtoan"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="text-blue-500 underline"
+                    >
+                      Github
+                    </a>
+                  </div>
+                  <div className="flex gap-2 text-md items-center">
+                    <div className="rounded-full border bg-card text-card-foreground shadow-xl p-2 backdrop-blur-md bg-opacity-30 border-opacity-50">
+                      <Mail />
+                    </div>
+                    <a
+                      href="mailto:toantrongvn@gmail.com"
+                      className="text-blue-500 underline"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      toantrongvn@gmail.com
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+          <div className={cn(cardClassName, "overflow-hidden relative")}>
+            <p className="text-2xl font-medium text-center">Your Brand</p>
+            <div
+              className={cn(
+                "h-[4rem] flex gap-2 items-center whitespace-nowrap justify-center text-[4rem] text-muted-foreground",
+                textHeading3ClassName
+              )}
+            >
+              {title}
+            </div>
+            <Input
+              className="w-full text-center"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          </div>
+
+          <div className={cn(cardClassName)}>
+            <div className="space-y-2">
+              <p className="text-2xl font-medium text-center">Free for all</p>
+              <p className="text-md text-center text-muted-foreground">
+                Chat Toolkit is an open-source project
+              </p>
+            </div>
+            <div className="p-4 border-violet-200 bg-violet-50 border-2 rounded-full shadow-xl">
+              <div className="rounded-full border bg-card text-card-foreground shadow-xl p-4 backdrop-blur-md bg-opacity-30 border-opacity-50">
+                <MessageCircleQuestion className="size-12" />
+              </div>
+            </div>
+            <p className="text-md text-center">
+              Contact me for get more support
+            </p>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
+      <div className="p-4 w-[420px] bg-[#f5f2f9]"></div>
     </div>
   );
 }
